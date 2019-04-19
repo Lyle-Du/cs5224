3dac031040277cf430de792eaf8f0e8839be9a0a
Du Qiu <Qiu.Du@dyson.com>
2019-04-19 23:13:43 +0800

    adjust layout

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -337,7 +337,7 @@ angular.module('starter.controllers', ['starter.services'])
         geodesic: true,
         strokeColor: '#FF0000',
         strokeOpacity: 1.0,
-        strokeWeight: 5,
+        strokeWeight: 2,
         icons: [{
           icon: lineSymbol,
           offset: '100%'
