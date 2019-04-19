f2fb3dd01db706e0e985197d3cff3d53c5af8a77
Wang Zili <Zili.Wang@dyson.com>
2019-04-19 23:01:30 +0800

    NA

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -211,6 +211,7 @@ angular.module('starter.controllers', ['starter.services'])
 
   $scope.isCardSelected = []
   function initIsCardSelected() {
+    isCardSelected = []
     $scope.searchResult.forEach(function(result) {
       $scope.isCardSelected.push(false)
     })
